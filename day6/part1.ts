2bdab070f4cf125b3563e75f8cb3d0e6aada5a3f
Chris Lail <chrislail@protonmail.com>
2025-12-06 19:23:48 -0800

    Add pre commit and pre push hooks and format

--- day6/part1.ts
+++ day6/part1.ts
@@ -1,26 +1,28 @@
-import * as fs from 'node:fs';
+import * as fs from "node:fs";
 
 function main() {
-    const input = fs.readFileSync("day6/input.txt", {encoding: "utf-8"});
-    const lines = input.split("\n").map(line => line.replaceAll(/\s+/g, " ").split(" "));
+  const input = fs.readFileSync("day6/input.txt", { encoding: "utf-8" });
+  const lines = input
+    .split("\n")
+    .map((line) => line.replaceAll(/\s+/g, " ").split(" "));
 
-    let answer = 0
-    for (let col = 0; col < lines[0]!.length; col++) {
-        const op = lines[lines.length - 1]![col]!;
+  let answer = 0;
+  for (let col = 0; col < lines[0]!.length; col++) {
+    const op = lines[lines.length - 1]![col]!;
 
-        let solution = Number(lines[0]![col])
-        for (let row = 1; row < lines.length - 1; row++) {
-            const num = Number(lines[row]![col])
-            if (op === "+") {
-                solution += num
-            } else {
-                solution *= num
-            }
-        }
-        answer += solution
+    let solution = Number(lines[0]![col]);
+    for (let row = 1; row < lines.length - 1; row++) {
+      const num = Number(lines[row]![col]);
+      if (op === "+") {
+        solution += num;
+      } else {
+        solution *= num;
+      }
     }
+    answer += solution;
+  }
 
-    console.log(`Part 1: ${answer}`)
+  console.log(`Part 1: ${answer}`);
 }
 
 main();
